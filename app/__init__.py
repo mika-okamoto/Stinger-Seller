cb1eee8a5271c23a5ef6873f8c7ac3fe418cecbe
Lindsey Zhang <67594565+lindsey-zh@users.noreply.github.com>
2023-10-13 23:03:50 -0400

    Added some card-fetching

--- app/__init__.py
+++ app/__init__.py
@@ -33,8 +33,6 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
-    # a simple page that says hello
     
     @app.route('/')
     def index():
