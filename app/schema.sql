d4ad74c6321a30aa68004d99cd31146a78a42557
mika-okamoto <mika.disneypins@gmail.com>
2023-10-13 22:16:00 -0400

    add seller and price

--- app/schema.sql
+++ app/schema.sql
@@ -4,6 +4,8 @@ CREATE TABLE item (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     name TEXT NOT NULL,
+    seller TEXT NOT NULL,
+    price FLOAT NOT NULL,
     description TEXT NOT NULL,
     image_path TEXT NOT NULL,
 );
